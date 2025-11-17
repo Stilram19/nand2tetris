1109f5250fb5d0aec6439a9cf23628744f21778c
Stilram19 <bednaouiomar19@gmail.com>
2025-11-17 04:26:32 +0100

    VM memory commands translated

--- software/project_07/VMCodewriter.py
+++ software/project_07/VMCodewriter.py
@@ -8,7 +8,6 @@ class VMCodewriter:
         self.ARG = 2
         self.THIS = 3
         self.THAT = 4
-        self.TEMP = 5
 
     # arithmetic commands
     def _add(self) -> str:
@@ -36,9 +35,120 @@ class VMCodewriter:
         return "\n".join(lines)
 
     def _push_pointer(self, arg: int) -> str:
-    def _pop_pointer(self, arg: int) -> str:
-    def _push_static(self, index: int) -> str:
-    def _push(self, base_addr: int, index: int) -> str:
-    def _pop(self, base_addr: int, index: int) -> str:
+        pointer = self.THIS if not arg else self.THAT
+        lines = [
+            f"// push pointer {arg}",
+            f"@{pointer}",
+            "D=M",
+            f"@{self.SP}",
+            "A=M",
+            "M=D",
+            f"@{self.SP}",
+            "M=M+1"
+        ]
+        return "\n".join(lines)
 
-    def writeCode(self, command: str[]):
+    def _pop_pointer(self, arg: int) -> str:
+        pointer = self.THIS if not arg else self.THAT
+        lines = [
+            f"// pop pointer {arg}",
+            f"@{self.SP}",
+            "M=M-1",
+            "A=M",
+            "D=M",
+            f"@{pointer}",
+            "M=D"
+        ]
+        return "\n".join(lines)
+
+    def _push_static(self, index: int) -> str:
+        static = self.fileName + '.' + str(index)
+        lines = [
+            f"// push static {index}",
+            f"@{static}",
+            "D=M",
+            f"@{self.SP}",
+            "A=M",
+            "M=D",
+            f"@{self.SP}",
+            "M=M+1"
+        ]
+        return "\n".join(lines)
+
+    def _pop_static(self, index: int) -> str:
+        static = self.fileName + '.' + str(index)
+        lines = [
+            f"// pop static {index}",
+            f"@{self.SP}",
+            "M=M-1",
+            "A=M",
+            "D=M",
+            f"@{static}",
+            "M=D"
+        ]
+        return "\n".join(lines)
+
+    def _push_temp(self, index: int) -> str:
+        addr = 5 + index
+        lines = [
+            f"// push temp {index}",
+            f"@{addr}",
+            "D=M",
+            f"@{self.SP}",
+            "A=M",
+            "M=D",
+            f"@{self.SP}",
+            "M=M+1"
+        ]
+        return "\n".join(lines)
+
+    def _pop_temp(self, index: int) -> str:
+        addr = 5 + index
+        lines = [
+            f"// pop temp {index}",
+            f"@{self.SP}",
+            "M=M-1",
+            "A=M",
+            "D=M",
+            f"@{addr}",
+            "M=D"
+        ]
+        return "\n".join(lines)
+
+    def _push(self, base_addr: int, index: int) -> str:
+        lines = [
+            f"// push segment {index}",
+            f"@{base_addr}",
+            "D=M",
+            f"@{index}",
+            "D=D+A",
+            "A=D",
+            "D=M",
+            f"@{self.SP}",
+            "A=M",
+            "M=D",
+            f"@{self.SP}",
+            "M=M+1"
+        ]
+        return "\n".join(lines)
+
+    def _pop(self, base_addr: int, index: int) -> str:
+        lines = [
+            f"// pop segment {index}",
+            f"@{base_addr}",
+            "D=M",
+            f"@{index}",
+            "D=D+A",
+            "@R13",
+            "M=D",
+            f"@{self.SP}",
+            "M=M-1",
+            "A=M",
+            "D=M",
+            "@R13",
+            "A=M",
+            "M=D"
+        ]
+        return "\n".join(lines)
+
+    def writeCode(self, command: list[str]):
